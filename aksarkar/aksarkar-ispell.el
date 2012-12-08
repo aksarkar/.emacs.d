f2bf95977d780578c9bf8529b91abd775b55afdf
Abhishek Sarkar <aksarkar@mit.edu>
2012-12-07 23:21:37 -0500

    Make hunspell pick the right encoding

--- aksarkar/aksarkar-ispell.el
+++ aksarkar/aksarkar-ispell.el
@@ -1,3 +1,6 @@
+(setq ispell-personal-dictionary "~/.hunspell_en_US")
 (setq ispell-program-name "hunspell")
+(defadvice ispell-get-coding-system (after ispell-always-pick-utf8 activate)
+  (setq ad-return-value 'utf-8))
 
 (provide 'aksarkar-ispell)
