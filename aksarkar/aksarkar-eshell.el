effa388b717654ccf765b232157fb9b9ff40b632
Abhishek Sarkar <aksarkar@mit.edu>
2013-12-22 21:24:59 -0500

    Keybinding for following links in eshell-mode

--- aksarkar/aksarkar-eshell.el
+++ aksarkar/aksarkar-eshell.el
@@ -1,6 +1,23 @@
 (require 'em-term)
 (setq eshell-cmpl-cycle-completions nil)
-(add-to-list 'eshell-visual-commands "tinyrss")
+(setq eshell-modules-list 
+      '(eshell-alias
+        eshell-basic
+        eshell-cmpl
+        eshell-dirs
+        eshell-glob
+        eshell-hist
+        eshell-ls
+        eshell-pred
+        eshell-prompt
+        eshell-script
+        eshell-term
+        eshell-unix))
 (add-to-list 'eshell-visual-commands "journalctl")
+
+(defun aksarkar-eshell-hook ()
+  (local-set-key (kbd "C-c C-v") 'browse-url-at-point))
+
+(add-hook 'eshell-mode-hook 'aksarkar-eshell-hook)
 
 (provide 'aksarkar-eshell)
