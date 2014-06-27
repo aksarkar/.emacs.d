d0f79a514fb4c9aefdcdd3af02903b3d154c31c7
Abhishek Sarkar <aksarkar@mit.edu>
2014-06-27 12:22:01 -0400

    Use python.el instead of python-mode.el

--- aksarkar/aksarkar-python.el
+++ aksarkar/aksarkar-python.el
@@ -1,14 +1,3 @@
-(require 'python-mode)
-(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
-
-(setq py-electric-comment-add-space-p nil)
-(setq py-shell-name "python3")
-(setq py-shell-switch-buffers-on-execute nil)
-(setq py-shell-switch-buffers-on-execute-p nil)
-(setq py-shell-toggle-1 "python2")
-(setq py-shell-toggle-2 "python3")
-(setq-default py-split-windows-on-execute-function 'split-window-horizontally)
-(setq py-start-run-py-shell nil)
-(setq py-verbose-p nil)
+(require 'python)
 
 (provide 'aksarkar-python)
