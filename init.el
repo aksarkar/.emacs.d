8faef47893bd8b403f46abac1556566def659e01
Abhishek Sarkar <aksarkar@mit.edu>
2015-06-08 18:16:53 -0400

    Update MELPA URL

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+                         ("melpa" . "http://melpa.org/packages/")))
 
 (require 'aksarkar-browse)
 (require 'aksarkar-eshell)
