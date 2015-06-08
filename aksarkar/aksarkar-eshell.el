9794415ee11286dd72afe12c2a1731dbdf32d2c8
Abhishek Sarkar <aksarkar@mit.edu>
2015-06-08 18:17:00 -0400

    Use helm for completion

--- aksarkar/aksarkar-eshell.el
+++ aksarkar/aksarkar-eshell.el
@@ -1,4 +1,6 @@
+(require 'eshell)
 (require 'em-term)
+
 (setq eshell-cmpl-cycle-completions nil)
 (setq eshell-modules-list 
       '(eshell-alias
@@ -16,7 +18,9 @@
 (add-to-list 'eshell-visual-commands "journalctl")
 
 (defun aksarkar-eshell-hook ()
-  (local-set-key (kbd "C-c C-v") 'browse-url-at-point))
+  (local-set-key (kbd "C-c C-v") 'browse-url-at-point)
+  (define-key eshell-mode-map (kbd "M-r") 'helm-eshell-history)
+  (define-key eshell-mode-map (kbd "<tab>") 'helm-esh-pcomplete))
 
 (add-hook 'eshell-mode-hook 'aksarkar-eshell-hook)
 
