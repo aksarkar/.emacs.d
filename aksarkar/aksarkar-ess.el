644301c5050bc88678abfb0f5ad19e4ad0a0faae
Abhishek Sarkar <aksarkar@mit.edu>
2015-06-08 18:03:11 -0400

    Configure ESS style for R

--- aksarkar/aksarkar-ess.el
+++ aksarkar/aksarkar-ess.el
@@ -3,5 +3,10 @@
 (ess-toggle-underscore nil)
 (setq inferior-R-program-name "bsub")
 (setq inferior-R-args "-q interactive -Is R --vanilla --quiet")
+(setq ess-default-style 'OWN
+      ess-eval-visibly nil
+      ess-eval-visibly-p nil
+      ess-first-continued-statement-offset 0
+      ess-continued-statement-offset 0)
 
 (provide 'aksarkar-ess)
