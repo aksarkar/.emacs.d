015c70493582b0dca73eaff007c9281e7bcda8e3
Abhishek Sarkar <aksarkar@mit.edu>
2015-07-21 22:29:07 -0400

    Configure helm-bibtex

--- aksarkar/aksarkar-helm.el
+++ aksarkar/aksarkar-helm.el
@@ -1,25 +1,39 @@
 (require 'helm-config)
-(require 'ac-helm)
+(require 'helm-bibtex)
 
-(global-set-key (kbd "M-x") 'helm-M-x)
-(global-set-key (kbd "M-y") 'helm-show-kill-ring)
-(global-set-key (kbd "M-`") 'helm-mark-ring)
+(helm-mode 1)
+
+(global-set-key (kbd "C-M-i") 'ac-complete-with-helm)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x b") 'helm-mini)
-(global-set-key (kbd "H-x f") 'helm-locate-library)
-(global-set-key (kbd "C-M-i") 'ac-complete-with-helm)
 (global-set-key (kbd "H-/") 'helm-semantic-or-imenu)
+(global-set-key (kbd "H-f") 'helm-locate-library)
+(global-set-key (kbd "H-b") 'helm-bibtex)
+(global-set-key (kbd "<f5>") 'helm-imenu)
+(global-set-key (kbd "M-`") 'helm-mark-ring)
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "M-y") 'helm-show-kill-ring)
 (define-key comint-mode-map (kbd "M-r") 'helm-comint-input-ring)
 (define-key lisp-mode-map (kbd "C-M-i") 'helm-lisp-completion-at-point)
 
-(setq helm-M-x-fuzzy-match t
-      helm-apropos-fuzzy-match t
-      helm-buffers-fuzzy-matching t
-      helm-completion-in-region-fuzzy-match t
-      helm-file-cache-fuzzy-match t
-      helm-lisp-fuzzy-completion t
-      helm-locate-fuzzy-match t
-      helm-recentf-fuzzy-match t
-      )
+(setq
+ helm-M-x-fuzzy-match t
+ helm-apropos-fuzzy-match t
+ helm-buffers-fuzzy-matching t
+ helm-completion-in-region-fuzzy-match t
+ helm-file-cache-fuzzy-match t
+ helm-lisp-fuzzy-completion t
+ helm-locate-fuzzy-match t
+ helm-recentf-fuzzy-match t
+ helm-bibtex-pdf-symbol "p"
+ helm-bibtex-pdf-open-function 'helm-open-file-with-default-tool
+ helm-bibtex-bibliography '("~/research/mit/reading/reading.bib")
+ helm-bibtex-library-path '("~/research/mit/reading" "~/misc/books/")
+ helm-bibtex-fallback-options
+ '(("PubMed" . "http://www.ncbi.nlm.nih.gov/pubmed/?term=%s")
+   ("bioRxiv" . "http://biorxiv.org/search/%s")
+   ("Google Scholar" . "http://scholar.google.co.uk/scholar?q=%s")
+   ("arXiv" . helm-bibtex-arxiv))
+ )
 
 (provide 'aksarkar-helm)
