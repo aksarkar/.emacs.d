783c0706aa9da94afd72174aec289ae4da9fd86e
Abhishek Sarkar <aksarkar@mit.edu>
2016-07-04 18:22:26 -0400

    Use hunspell

--- aksarkar/aksarkar-ispell.el
+++ aksarkar/aksarkar-ispell.el
@@ -1,4 +1,4 @@
-(setq ispell-program-name "aspell")
+(setq ispell-program-name "hunspell")
 (defadvice ispell-get-coding-system (after ispell-always-pick-utf8 activate)
   (setq ad-return-value 'utf-8))
 
