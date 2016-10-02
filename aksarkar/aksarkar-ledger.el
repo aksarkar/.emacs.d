5d04303ec640ba82adec38d9c0252302b161e97b
Abhishek Sarkar <aksarkar@mit.edu>
2016-10-01 22:35:24 -0400

    Update reports

--- aksarkar/aksarkar-ledger.el
+++ aksarkar/aksarkar-ledger.el
@@ -1,7 +1,8 @@
 (require 'ledger-mode)
 
 (setq ledger-reports
-      '(("reg" "ledger -f %(ledger-file) reg checking payable")
+      '(("reg" "ledger -f %(ledger-file) reg checking payable --cleared")
+        ("reg-against-budget" "ledger -f  %(ledger-file) reg expenses -p \"this month\" --add-budget --cleared")
         ("budget" "ledger -f %(ledger-file) -p \"this month\" budget")))
 
 (provide 'aksarkar-ledger)
