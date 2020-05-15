fccc368fb3f60ef2048587908b434290698fbdf1
Abhishek Sarkar <aksarkar@alum.mit.edu>
2020-05-14 22:58:30 -0500

    Update R config

--- aksarkar/aksarkar-ess.el
+++ aksarkar/aksarkar-ess.el
@@ -1,9 +1,7 @@
 (require 'ess-site)
 
 (ess-toggle-underscore nil)
-(setq inferior-R-program-name "bsub")
-(setq inferior-R-args "-q interactive -Is R --vanilla --quiet")
-(setq ess-default-style 'OWN
+(setq ess-default-style 'RStudio
       ess-eval-visibly nil
       ess-eval-visibly-p nil
       ess-first-continued-statement-offset 0
