b81fa2de2c023e9b7e349fd3c4622ded5ef5e2c3
Abhishek Sarkar <aksarkar@alum.mit.edu>
2022-06-04 18:51:05 -0500

    Update formatting of python-mode configuration

--- aksarkar/aksarkar-python.el
+++ aksarkar/aksarkar-python.el
@@ -1,14 +1,16 @@
 (require 'python-mode)
 (add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
 
-(setq py-electric-comment-add-space-p nil)
-(setq py-shell-name "python3")
-(setq py-shell-switch-buffers-on-execute nil)
-(setq py-shell-switch-buffers-on-execute-p nil)
-(setq py-shell-toggle-1 "python2")
-(setq py-shell-toggle-2 "python3")
+(setq py-electric-comment-add-space-p nil
+      py-shell-name "python3"
+      py-shell-switch-buffers-on-execute nil
+      py-shell-switch-buffers-on-execute-p nil
+      py-shell-toggle-1 "python2"
+      py-shell-toggle-2 "python3"
+      py-start-run-py-shell nil
+      py-verbose-p nil
+      python-indent-offset 2)
 (setq-default py-split-windows-on-execute-function 'split-window-horizontally)
-(setq py-start-run-py-shell nil)
-(setq py-verbose-p nil)
+
 
 (provide 'aksarkar-python)
