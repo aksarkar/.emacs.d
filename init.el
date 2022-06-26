1c757792733fa06f935735c841f40ce2448bad57
Abhishek Sarkar <aksarkar@alum.mit.edu>
2022-06-26 10:38:01 -0500

    Don't use .authinfo

--- init.el
+++ init.el
@@ -28,6 +28,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(helm-completion-style 'emacs)
  '(helm-external-programs-associations
    '(("djvu" . "evince")
